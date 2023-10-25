9a3029b96b6c09c1420c2baac4fc5846fa418b0f
Iffy Eleweke <omaleweks@gmail.com>
2023-10-24 22:51:49 -0400

    Created a 404 route in our app

--- Week 1/week-1-day-2/exp_demo6.js
+++ Week 1/week-1-day-2/exp_demo6.js
@@ -4,11 +4,15 @@ var app = express();
 app.get('/home', (req, res) => {
     res.send('Home Page');
 });
+
 app.get('/about', (req, res) => {
     res.send('About');
 });
-// For invalid routes
 
+// For invalid routes
+app.get('*', (req, res) => {
+    res.send('404! This is an invalid URL.');
+});
 
 app.listen(3000);
 
