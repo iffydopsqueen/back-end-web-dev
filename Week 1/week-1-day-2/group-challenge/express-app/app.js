67eb4f650e0aeed975a60f63e979ff3bed067f90
Iffy Eleweke <omaleweks@gmail.com>
2023-10-25 00:20:46 -0400

    Created an ExpressJS app that displays a username and password

---
+++ Week 1/week-1-day-2/group-challenge/express-app/app.js
@@ -0,0 +1,20 @@
+// Require the 'express' module to create an Express application
+var express = require('express');
+
+// Require the 'path' module to work with file paths
+var path = require('path');
+
+// Create an Express application
+var app = express();
+
+// Define the port number & hostname on which the app will listen for incoming requests
+var port = 3000;
+var hostname = 'localhost';
+
+// Serve static files (like the "index.html" file) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the Express app and listen on the specified port
+app.listen(port, hostname, () => {
+    console.log(`App is running on http://${hostname}:${port}`);
+});
