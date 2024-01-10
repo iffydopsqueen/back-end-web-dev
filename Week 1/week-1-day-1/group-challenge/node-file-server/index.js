c6b09c6cb507fe40adb76d260e48e5a448be2ef3
Iffy Eleweke <omaleweks@gmail.com>
2024-01-10 10:33:17 -0500

    Modified my node-file-server file used to create an http server

--- Week 1/week-1-day-1/group-challenge/node-file-server/index.js
+++ Week 1/week-1-day-1/group-challenge/node-file-server/index.js
@@ -7,41 +7,56 @@ var fs = require('fs');
 // Import the 'path' module to manipulate file paths.
 var path = require('path');
 
-// Define the port on which the server will listen.
+// Define the hostname & port on which the server will listen.
+var hostname = 'localhost';
 var port = 5000; // You can use any port you prefer
 
 // Create an HTTP server and define a callback function that handles requests and responses.
 var server = http.createServer((req, res) => {
-  if (req.url === '/') {  // Check if the requested URL is the root URL ('/').
-    
-    // Construct the absolute file path to the 'index.html' file in the current directory.
-    var htmlFilePath = path.join(__dirname, 'index.html');
+    console.log(`Request for ${req.url} by method ${req.method}`);
 
-    // Read the contents of the 'index.html' file using UTF-8 encoding.
-    fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+    if (req.method === 'GET') {
+        var fileUrl = req.url;
 
-      // If there is an error reading the file, respond with a 500 Internal Server Error.
-      if (err) {
-        res.statusCode = 500;
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        var filePath = path.resolve('./' + fileUrl);
+        var fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            // Check if the requested URL is for an HTML page.
+            fs.access(filePath, function (err) {
+                if (err) {
+                    // If there is an error reading the file, respond with a 500 Internal Server Error.
+                    res.statusCode = 500;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Internal Server Error</h1></body></html>`);
+                    return;
+                }
+
+                // If the file is read successfully, respond with a 200 OK status and serve the HTML content.
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            // If the requested URL is not for an HTML page, respond with a 404 Not Found status.
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Not Found</h1></body></html>`);
+        }
+    } else {
+        // If the request method is not GET, respond with a 404 Not Found status.
+        res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
-        res.end(`<html><body><h1>Internal Server Error</h1></body></html>`);
-        return;
-      }
-
-      // If the file is read successfully, respond with a 200 OK status and serve the HTML content.
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data);
-    });
-  } else {
-    // If the requested URL is not the root URL, respond with a 404 Not Found status.
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<html><body><h1>Not Found</h1></body></html>`);
-  }
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
 });
 
-// Start the server and listen on the specified port.
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+// Start the server and listen on the specified port and hostname.
+server.listen(port, hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}/`);
 });
