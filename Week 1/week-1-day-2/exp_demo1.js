d8061d14093a55d926db8ed4b21d9cc3418e4e66
Iffy Eleweke <omaleweks@gmail.com>
2024-01-10 23:58:48 -0500

    Updated some exercise files and added an ExpressJS app folder for group challenge

--- Week 1/week-1-day-2/exp_demo1.js
+++ Week 1/week-1-day-2/exp_demo1.js
@@ -1,8 +1,12 @@
+// how to invoke Express
 var express = require('express');
 var app = express();
 
+var port = 3000;
+
+// route
 app.get('/', function(req, res) {
     res.send("Hello world!");
 });
 
-app.listen(3000);
+app.listen(port);
--- Week 1/week-1-day-2/exp_demo4.js
+++ Week 1/week-1-day-2/exp_demo4.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/home', (req, res) => {
+app.get('/home', function(req, res) {
     res.send('Home Page');
 });
 
-app.get('/about', (req, res) => {
-    res.send('About');
+app.get('/about', function(req, res) {
+    res.send('About Page');
 });
 
 app.listen(3000);
