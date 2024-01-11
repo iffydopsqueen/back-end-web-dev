9f2a46a285375fc4ce3cf42f9733d86d7979bbb8
Iffy Eleweke <omaleweks@gmail.com>
2024-01-11 00:22:51 -0500

    Updated changes

--- Week 1/week-1-day-2/group-challenge/express-app/app.js
+++ Week 1/week-1-day-2/group-challenge/express-app/app.js
@@ -1,11 +1,10 @@
 // Require the 'express' module to create an Express application
 var express = require('express');
+// Create an Express application
+var app = express();
 
 // Require the 'path' module to work with file paths
 var path = require('path');
-
-// Create an Express application
-var app = express();
 
 // Define the port number & hostname on which the app will listen for incoming requests
 var port = 3000;
@@ -15,6 +14,6 @@ var hostname = 'localhost';
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the Express app and listen on the specified port
-app.listen(port, hostname, () => {
+app.listen(port, hostname, function() {
     console.log(`App is running on http://${hostname}:${port}`);
 });
