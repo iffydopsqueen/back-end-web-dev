e6b8c9f500d74853370b0d723243bda873b21735
Iffy Eleweke <omaleweks@gmail.com>
2024-01-12 12:44:35 -0500

    Fixed the 'submit' button response and 'logging' my data output to the terminal

--- Week 1/week-1-day-3/m3-w1-lab-assignment/demo-node-app/routes/index.js
+++ Week 1/week-1-day-3/m3-w1-lab-assignment/demo-node-app/routes/index.js
@@ -8,10 +8,26 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', 
-    
+    [
+    check('name')
+        .isLength({ min: 1 })
+        .withMessage('Please enter a name'),
+    check('email')
+        .isLength({ min: 1 })
+        .withMessage('Please enter an email'),
+    ],
     function(req, res) {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form' });
+        console.log(req.body);
+        var errors = validationResult(req);
+        if (errors.isEmpty()) {
+            res.send('Thank you for your registration!');
+        } else {
+            res.render('form', {
+                title: 'Registration form',
+                errors: errors.array(),
+                data: req.body,
+            });
+        }
 });
 
 module.exports = router;
