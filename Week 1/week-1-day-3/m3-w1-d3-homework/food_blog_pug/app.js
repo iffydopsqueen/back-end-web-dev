10a3b1e662ae6bf988061a37a24fbfb5863ee385
Iffy Eleweke <omaleweks@gmail.com>
2024-01-16 19:06:13 -0500

    Added comments to my code for future reference

--- Week 1/week-1-day-3/m3-w1-d3-homework/food_blog_pug/app.js
+++ Week 1/week-1-day-3/m3-w1-d3-homework/food_blog_pug/app.js
@@ -1,18 +1,31 @@
+// Imports the Express framework
+// This allows you to create an HTTP server and define routes.
 var express = require("express");
+
+// Imports the "path" module
+// This provides utilities for working with file and directory paths.
 var path = require("path");
+
+// Imports the "router" from the "server.js" file located in the "routes" directory
 var serverRouter = require("./routes/server");
+
+// Creates an instance of the Express application, which is used to configure and run the server
 var app = express();
 
-// Serve static files (css, js, images)
+// Serve static files (css, js, images) from the "public" directory
+// This line sets up a middleware to handle requests for static files and 
+// maps them to the /public URL path.
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Set the views directory and use Pug as the view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Mounts the "serverRouter" middleware at the "/components" path
+// This means that any requests to "/components" will be handled by the "routes" defined in "serverRouter".
 app.use("/components", serverRouter);
 
-// Define routes
+// Defines a route for the root path ("/") that renders the Pug template named "food_blog"
 app.get("/", function (req, res) {
   res.render("food_blog");
 });
